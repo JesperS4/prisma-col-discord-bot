42899ac0ee2c65f46e2b40d48a0b8f9e65f9604e
Jesperr <400041661@st.roc.a12.nl>
2022-08-22 00:00:27 +0200

    added some bullshit

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const discord = require('discord.js')
+const mysql = require('mysql')
 const config = require('./botconfig.json')
 
 
 const client = new discord.Client()
 
 
-client.on("ready", () => {
+const Functions = {}
+const Data = {}
+
+var connection;
+
+Functions.startUp = async () => {
     console.log("[DEBUG] BOT STARTING UP")
     let ticketCount = 0
     client.user.setActivity({name: `${ticketCount} orders`, type: "WATCHING"})
+
+    // connecting sql
+    // connection = mysql.createConnection({
+    // host     : 'localhost',
+    // user     : 'root',
+    // password : '',
+    // database : 'bot'
+    // });
+
+    // await connection.connect();
+
+    return;
+}
+
+Functions.setupData = async () => {
+
+}
+
+client.on("ready", async () => {
+    await Functions.startUp();
+    await Functions.setupData();
 })
 
 
