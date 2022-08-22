784e11f475c93cad5a9e9dd237b66c29c1f2c1cb
Jesperr <400041661@st.roc.a12.nl>
2022-08-22 19:48:17 +0200

    made ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,15 +1,48 @@
 const discord = require("discord.js")
 
 module.exports.run = async(bot, message, args) =>{
+    try {
+        const noPermsEmbed = new discord.MessageEmbed()
+        .setTitle("Geen permission")
+        .setDescription("Je hebt geen permissie tot dit commando!")
+        .setFooter("Prisma Scripts ©")
+        .setTimestamp()
 
-    const noPermsEmbed = new discord.MessageEmbed()
-    .setTitle("Geen permission")
-    .setDescription("Je hebt geen permissie tot dit commando!")
-    .setFooter("Prisma Scripts ©")
-    .setTimestamp()
+    
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(noPermsEmbed)
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!args[0]) return message.channel.send('Kan de gebruiker niet vinden, `bericht wordt automatisch verwijderd in 2 seconden`').then(x => {
+            setTimeout(() => {
+                x.delete()
+            }, 2000 )
+        });
+    
+        if(!member) return message.channel.send('Kan de gebruiker niet vinden, `bericht wordt automatisch verwijderd in 2 seconden`').then(x => {
+            setTimeout(() => {
+                x.delete()
+            }, 2000 )
+        });
+    
+        if(!member.bannable) return message.channel.send('Ik kan deze member niet eens bannen, `bericht wordt automatisch verwijderd in 2 seconden`').then(x => {
+            setTimeout(() => {
+                x.delete()
+            }, 2000 )
+        });;
+    
+        let reason = args.slice(1).join(" ") ? args.slice(1).join(" ") : "Geen reden";
+    
+        member.ban(reason)
 
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(noPermsEmbed)
+        const banEmbed = new discord.MessageEmbed()
+        .setTitle("Succesvol gebanned")
+        .setDescription("Je hebt " + member + " succesvol gebanned, met de reden: " + reason + ".")
+        .setFooter("Prisma Scripts ©")
+        .setTimestamp()
+    
 
+    } catch(e) {
+        return message.channel.send("Er is een error, error: `"+e+"`")
+    }
 
 
 
